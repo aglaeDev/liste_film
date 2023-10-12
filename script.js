b1a3ee28b23b32f8a9309eccdfb1a80eca40adfb
aglaeDev <a.gourdet.dev@gmail.com>
2023-10-12 18:42:15 +0200

    add search

---
+++ script.js
@@ -0,0 +1,14 @@
+const apikeys = "253c4db"
+
+const searchMovie = document.getElementById("movie-search");
+const searchBar = document.getElementById("search-bar");
+const filmList = document.getElementById("film-list");
+const modal = document.getElementById("modal");
+const modalTitle = document.getElementById("modal-title");
+const modalRelease = document.getElementById("modal-release");
+const modalPlot = document.getElementById("modal-plot");
+const closeBtn = document.querySelector(".close");
+
+
+
+var url = "https://www.omdbapi.com/?i=tt3896198&apikey=253c4db"
